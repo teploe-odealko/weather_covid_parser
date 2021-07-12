affd7d15b95b453bd77bb38db9b5bae5f595ceab
Динар <din.latypov@gmail.com>
2021-07-12 12:57:06 +0300

    request_html version

--- covid.py
+++ covid.py
@@ -12,18 +12,27 @@ from openpyxl.formula.translate import Translator
 from openpyxl.utils.cell import get_column_letter
 import dateparser
 from copy import copy
+from openpyxl.formatting.rule import FormulaRule
+
 import re
+from openpyxl.styles import Color, PatternFill, Font, Border
 
 urllib3.disable_warnings()
 
-SHEET_NAME_CASES = 'Случаев'
-SHEET_NAME_BASE = 'База РФ'
 
 URL = 'https://yastat.net/s3/milab/2020/covid19-stat/data/v10/deep_data.json'
 REGION_COLUMN = 2
 REGION_ROWS_RANGE = (2, 87)
-WEEKLY_REPORT_DAY = 4
-
+WEEKLY_REPORT_DAY = 5
+DAYS_OF_THE_WEEK_MAP = {
+    1: 'ПН',
+    2: 'ВТ',
+    3: 'СР',
+    4: 'ЧТ',
+    5: 'ПТ',
+    6: 'СБ',
+    7: 'ВС',
+}
 
 def normolize_string(string: str):
     string = string.strip().lower().translate({ord('.'): None})
@@ -40,10 +49,10 @@ def normolize_string(string: str):
     return string
 
 
-def fill_сases_sheet(ws: Worksheet, regions: dict, dates):
+def fill_сases_sheet(ws: Worksheet, info: dict):
     OVERALL_CASES_FORMULA_ROW = 88
 
-    last_available_date = datetime.strptime(dates[-1], '%Y-%m-%d')
+    last_available_date = datetime.strptime(info['dates'][-1], '%Y-%m-%d')
     last_column = ws.max_column
     # print("last column", last_column)
     last_table_date = ws.cell(row=1, column=last_column).value
@@ -55,10 +64,9 @@ def fill_сases_sheet(ws: Worksheet, regions: dict, dates):
         for row in range(*REGION_ROWS_RANGE):
             name = ws.cell(row=row, column=REGION_COLUMN).value
             key = normolize_string(name)
-            if key in regions:
+            if key in info['data']:
                 ws.cell(row=row, column=(last_column + i)).value = \
-                    regions[key]['cases'][(i - 1) - new_days_amount][0]
-
+                    info['data'][key]['cases'][(i - 1) - new_days_amount][0]
             else:
                 raise KeyError(key)
 
@@ -66,14 +74,12 @@ def fill_сases_sheet(ws: Worksheet, regions: dict, dates):
         last_cell_id = get_column_letter(ws.max_column - 1) + str(OVERALL_CASES_FORMULA_ROW)
         next_cell_id = get_column_letter(ws.max_column) + str(OVERALL_CASES_FORMULA_ROW)
         ws[next_cell_id] = Translator(formula, origin=last_cell_id).translate_formula(next_cell_id)
-    return {'new_days_amount': new_days_amount,
-            'last_date': last_table_date,
-            'current_date': last_table_date + timedelta(days=new_days_amount)}
+
+    info.update({'new_days_amount': new_days_amount})
 
 
-def get_regions_info() -> Tuple[dict, list]:
-    final = {}
-    # regions_short_name = {}
+def get_regions_info() -> dict:
+    final_region_data = {}
 
     session = HTMLSession()
     response: dict = session.get(URL, verify=False).json()
@@ -84,10 +90,12 @@ def get_regions_info() -> Tuple[dict, list]:
         short_name = regions[region]['info']['short_name']
         regions[region]['info'].pop('name', None)
         regions[region]['info'].pop('short_name', None)
-        final.update(dict.fromkeys([normolize_string(full_name), normolize_string(short_name)], region))
-        # regions_full_name[normolize_string(full_name)] = regions[region]
-        # regions_short_name[normolize_string(short_name)] = regions[region]
-    return final, dates
+        final_region_data.update(
+            dict.fromkeys([normolize_string(full_name), normolize_string(short_name)], regions[region]))
+    print(dates[-1])
+    return {'data': final_region_data,
+            'dates': dates,
+            'last_date': datetime.strptime(dates[-1], '%Y-%m-%d')}
 
 
 def continue_formula_right(ws: Worksheet, rows: list, column: int):
@@ -96,7 +104,9 @@ def continue_formula_right(ws: Worksheet, rows: list, column: int):
         last_cell_id = get_column_letter(column - 1) + str(row)
         next_cell_id = get_column_letter(column) + str(row)
         ws[next_cell_id] = Translator(formula, origin=last_cell_id).translate_formula(next_cell_id)
+        # print(last_cell_id, next_cell_id)
         ws[next_cell_id]._style = copy(ws[last_cell_id]._style)
+        # print(ws[last_cell_id]._style, ws[next_cell_id]._style)
 
 
 def continue_date_right(ws: Worksheet, rows: list):
@@ -128,60 +138,67 @@ def continue_number_right(ws: Worksheet, rows: list):
         ws[next_cell_id]._style = copy(ws[last_cell_id]._style)
 
 
-def gain_sheet(ws: Worksheet, info_dict: dict):
+def gain_sheet(ws: Worksheet, info: dict):
     NEW_COLUMN_NUM_GAIN = [c.value for c in ws[1]].index(None) + 1
 
-    for i in range(info_dict['new_days_amount']):
+    for i in range(info['new_days_amount']):
         continue_date_right(ws, [1])
         continue_formula_right(ws, list(range(2, 96)), NEW_COLUMN_NUM_GAIN + i)
 
+    info['last_col_in_gain_sheet'] = [c.value for c in ws[1]].index(None)
 
-def daily_gain_sheet(ws: Worksheet, info_dict: dict):
+
+def daily_region_gain_sheet(ws: Worksheet, info: dict):
     NEW_COLUMN_NUM_DAILY_GAIN = ws.max_column + 1
 
-    for i in range(info_dict['new_days_amount']):
+    for i in range(info['new_days_amount']):
         continue_date_right(ws, [1, 20, 35])
         continue_formula_right(ws,
                                list(range(2, 19)) + list(range(21, 34)) + list(range(36, 49)),
                                NEW_COLUMN_NUM_DAILY_GAIN + i)
 
-def weekly_gain_sheet(wb, info_dict: dict):
-    ws_weekly_gain = wb['Прирост нед']
-    print(info_dict['current_date'].weekday() + 1, WEEKLY_REPORT_DAY)
-    if (info_dict['current_date'].weekday() + 1) == WEEKLY_REPORT_DAY:
-        new_col = [c.value for c in ws_weekly_gain[1]].index(None) + 1
-        print(new_col)
-        ws_weekly_gain.insert_cols(new_col)
-        continue_number_right(ws_weekly_gain, [1])
-        continue_formula_right(ws_weekly_gain, list(range(2, 94)), new_col)
-        for row in range(2, 87):
-            raw_formula = ws_weekly_gain.cell(row=row, column=new_col).value
-            tokenized_formula = Tokenizer(raw_formula)
-            last_col_in_gain_sheet = [c.value for c in wb['Прирост'][1]].index(None)
 
-            tokenized_formula.items[1].value = re.sub(r'(?<=:\$)\D*',
-                                                      get_column_letter(last_col_in_gain_sheet),
-                                                      tokenized_formula.items[1].value)
-            tokenized_formula.items[3].value = re.sub(r'(?<=:\$).*(?=\$)',
-                                                      get_column_letter(last_col_in_gain_sheet),
-                                                      tokenized_formula.items[3].value)
-            new_formula = ''.join(token.value for token in tokenized_formula.items)
-            ws_weekly_gain.cell(row=row, column=new_col).value = fr'={new_formula}'
+def weekly_gain_sheet(ws: Worksheet, info: dict):
+    print(info['last_date'].weekday() + 1, WEEKLY_REPORT_DAY)
+    new_col = [c.value for c in ws[1]].index(None) + 1
+    ws.insert_cols(new_col)
+    continue_number_right(ws, [1])
+    continue_formula_right(ws, list(range(2, 94)), new_col)
+    for row in range(2, 87):
+        raw_formula = ws.cell(row=row, column=new_col).value
+        tokenized_formula = Tokenizer(raw_formula)
+        last_col_in_gain_sheet = info['last_col_in_gain_sheet']
 
-def cases_sheet(wb):
-    regions_info = get_regions_info()
-    print(regions_info)
-    info_dict = fill_сases_sheet(wb[SHEET_NAME_CASES], *regions_info)
+        tokenized_formula.items[1].value = re.sub(r'(?<=:\$)\D*',
+                                                  get_column_letter(last_col_in_gain_sheet),
+                                                  tokenized_formula.items[1].value)
+        tokenized_formula.items[3].value = re.sub(r'(?<=:\$).*(?=\$)',
+                                                  get_column_letter(last_col_in_gain_sheet),
+                                                  tokenized_formula.items[3].value)
+        new_formula = ''.join(token.value for token in tokenized_formula.items)
+        ws.cell(row=row, column=new_col).value = fr'={new_formula}'
 
-    gain_sheet(wb['Прирост'], info_dict)
-    daily_gain_sheet(wb['По рег прис (сут)'], info_dict)
-    weekly_gain_sheet(wb, info_dict)
 
-        # continue_number_right(ws_weekly_gain, )
+def tpr_weekly_gain_sheet(ws: Worksheet, info: dict):
+    new_col = [c.value for c in ws[1]].index(None) + 1
+    ws.insert_cols(new_col)
+    continue_number_right(ws, [1])
+    continue_formula_right(ws, list(range(2, 88)), new_col)
+
+
+# {}
+
+# def cases_sheet(wb):
+#
+#
+    # continue_number_right(ws_weekly_gain, )
+
 
 def continue_formula_n_down(ws: Worksheet, columns: list, row: int, n: int):
     for column in columns:
+
         formula = ws.cell(row=(row - n), column=column).value
+        # print(row-n, column, formula)
         last_cell_id = get_column_letter(column) + str(row - n)
         next_cell_id = get_column_letter(column) + str(row)
         ws[next_cell_id] = Translator(formula, origin=last_cell_id).translate_formula(next_cell_id)
@@ -192,67 +209,142 @@ def continue_formula_down(ws: Worksheet, columns: list, row: int):
     continue_formula_n_down(ws, columns, row, 1)
 
 
-def base_rf_sheet(ws: Worksheet):
-    STOP_COVID_URL = 'https://стопкоронавирус.рф/information/'
-    NEW_ROW_NUM = max((c.row for c in ws['B'] if c.value is not None)) + 1
-    session = HTMLSession()
-    response = session.get(STOP_COVID_URL, verify=False)
-    response.html.render()
+# def get_first_clear_column_in_row(row):
+#     return max((c.row for c in column if c.value is not None)) + 1
 
-    actual_date = response.html.xpath('//small/text()')[0]
-    actual_date = dateparser.parse(''.join(actual_date.split()[3:]))
-    print(actual_date.weekday())
-    ws.cell(row=NEW_ROW_NUM, column=1).value = actual_date
+
+def get_first_clear_row_in_column(column):
+    return max((c.row for c in column if c.value is not None)) + 1
+
+def base_rf_sheet(ws: Worksheet, info: dict):
+    NEW_ROW_NUM = get_first_clear_row_in_column(ws['B'])
+
+    ws.cell(row=NEW_ROW_NUM, column=1).value = info['last_date']
     ws.cell(row=NEW_ROW_NUM, column=1).number_format = 'DD.MM'
-
-    rows = response.html.xpath('//h3[@class="cv-stats-virus__item-value"]/text()')
-    statistics = [int(''.join(row.strip('\n ').split())) for row in rows]
-    useful_statistics = [statistics[2], statistics[4], statistics[0]]
-    for i, stat in enumerate(useful_statistics):
-        ws.cell(row=NEW_ROW_NUM, column=2 + i).value = useful_statistics[i]
+    for i, stat in enumerate(info['general']):
+        ws.cell(row=NEW_ROW_NUM, column=2 + i).value = info['general'][i]
         ws.cell(row=NEW_ROW_NUM, column=2 + i)._style = copy(ws.cell(row=NEW_ROW_NUM - 1, column=2 + i)._style)
-
     continue_formula_down(ws,
                           list(range(5, 20)) + [26] + list(range(28, 36)),
                           NEW_ROW_NUM)
 
-    if actual_date.weekday() == 6:  # if it's sunday
-        continue_formula_n_down(ws,
-                                list(range(20, 25)),
-                                NEW_ROW_NUM,
-                                7)
-        continue_formula_n_down(ws,
-                                list(range(20, 23)),
-                                NEW_ROW_NUM + 1,
-                                7)
-        continue_formula_n_down(ws,
-                                [27],
-                                NEW_ROW_NUM,
-                                7)
-
-    return actual_date
+    if info['last_date'].weekday() + 1 == WEEKLY_REPORT_DAY:  # if it's sunday
+        continue_formula_n_down(ws, list(range(20, 25))+[27], NEW_ROW_NUM, 7)
+        continue_formula_n_down(ws, list(range(20, 23)), NEW_ROW_NUM + 1, 7)
 
 
-def date_week_sheet(ws: Worksheet, actual_date: datetime):
-    NEW_ROW_NUM = max((c.row for c in ws['A'] if c.value is not None)) + 1
-    if actual_date.weekday() == WEEKLY_REPORT_DAY:
-        ws.cell(row=NEW_ROW_NUM, column=1).value = ws.cell(row=NEW_ROW_NUM - 1, column=1).value + 1
-        continue_formula_down(ws,
-                              list(range(1, 3)),
-                              NEW_ROW_NUM)
+def date_week_sheet(ws: Worksheet, info: dict):
+    NEW_ROW_NUM = get_first_clear_row_in_column(ws['A'])
+    print(info['last_date'].weekday() + 1, WEEKLY_REPORT_DAY)
+    ws.cell(row=NEW_ROW_NUM, column=1).value = ws.cell(row=NEW_ROW_NUM - 1, column=1).value + 1
+    print(NEW_ROW_NUM)
+    continue_formula_down(ws, list(range(2, 4)), NEW_ROW_NUM)
 
 
 # def daily_gain_sheet(ws: Worksheet, actual_date: datetime):
 
+def get_general_info(info: dict):
+    STOP_COVID_URL = 'https://стопкоронавирус.рф/information/'
+    session = HTMLSession()
+    response = session.get(STOP_COVID_URL, verify=False)
+    response.html.render()
+
+    rows = response.html.xpath('//h3[@class="cv-stats-virus__item-value"]/text()')
+    statistics = [int(''.join(row.strip('\n ').split())) for row in rows]
+    useful_statistics = [statistics[2], statistics[4], statistics[0]]
+    info.update({'general': useful_statistics})
+
+
+
+def parse_info():
+    info = get_regions_info()
+    # {dates: [list of available dates], data: {'*REGION_NAME*': {*REGION_INFO*}}}
+    get_general_info(info)
+    return info
+
+
+def weekly_region_gain_sheet(ws: Worksheet, info: dict):
+    new_column = ws.max_column + 1
+
+    continue_number_right(ws, [1, 20])
+    continue_formula_right(ws,
+                           list(range(2, 17)) + list(range(21, 34)),
+                           new_column)
+
+def gain_7day_sheet(ws: Worksheet, info: dict):
+    new_column = [c.value for c in ws[1]].index(None) + 1
+
+    for i in range(info['new_days_amount']):
+        ws.insert_cols(new_column+i)
+        continue_date_right(ws, [1])
+        continue_formula_right(ws, list(range(2, 94)), new_column + i)
+
+
+def rt_sheet(ws: Worksheet, info: dict):
+    new_column = [c.value for c in ws[2]].index(None) + 1
+
+    for i in range(info['new_days_amount']):
+        ws.insert_cols(new_column+i)
+        continue_date_right(ws, [2])
+        continue_formula_right(ws, list(range(3, 89)), new_column + i)
+
+
+def delta_day_sheet(ws: Worksheet, info: dict):
+    new_column = ws.max_column + 1
+    bigger_zero_fill = PatternFill(start_color='F1CCB1', end_color='F1CCB1', fill_type='solid')
+    lower_zero_fill = PatternFill(start_color='E4EFDC', end_color='E4EFDC', fill_type='solid')
+
+    for i in range(info['new_days_amount']):
+        continue_date_right(ws, [1])
+        ws.cell(row=2, column=new_column + i).value = DAYS_OF_THE_WEEK_MAP.get(
+            ws.cell(row=1, column=new_column + i).value.weekday() + 1
+        )
+        last_cell_id = get_column_letter(new_column + i - 1) + str(2)
+        next_cell_id = get_column_letter(new_column + i) + str(2)
+        ws[next_cell_id]._style = copy(ws[last_cell_id]._style)
+
+        continue_formula_right(ws, list(range(3, 98)), new_column + i)
+        cell_ids_formatting = '{}3:{}87'.format(get_column_letter(new_column + i), get_column_letter(new_column + i))
+        ws.conditional_formatting.add(cell_ids_formatting, FormulaRule(formula=[f'{next_cell_id}>0'], fill=bigger_zero_fill))
+        ws.conditional_formatting.add(cell_ids_formatting, FormulaRule(formula=[f'{next_cell_id}<0'], fill=lower_zero_fill))
+
+
+def msc_sp_sheet(ws: Worksheet, info: dict):
+    new_column = ws.max_column + 1
+    # bigger_zero_fill = PatternFill(start_color='F1CCB1', end_color='F1CCB1', fill_type='solid')
+    # lower_zero_fill = PatternFill(start_color='E4EFDC', end_color='E4EFDC', fill_type='solid')
+
+    for i in range(info['new_days_amount']):
+        continue_date_right(ws, [1])
+        ws.cell(row=2, column=new_column + i).value = DAYS_OF_THE_WEEK_MAP.get(
+            ws.cell(row=1, column=new_column + i).value.weekday() + 1
+        )
+        last_cell_id = get_column_letter(new_column + i - 1) + str(2)
+        next_cell_id = get_column_letter(new_column + i) + str(2)
+        ws[next_cell_id]._style = copy(ws[last_cell_id]._style)
+        continue_formula_right(ws, list(range(3, 98)), new_column + i)
 
 def main(filename: str):
     wb = openpyxl.load_workbook(filename)
-    cases_sheet(wb)
-    actual_date = base_rf_sheet(wb[SHEET_NAME_BASE])
 
-    date_week_sheet(wb['Дата-неделя'], actual_date)
+    info = parse_info()
 
-    # daily_gain_sheet(wb['По рег прис (сут)'], actual_date)
+    fill_сases_sheet(wb['Случаев'], info)
+    gain_sheet(wb['Прирост'], info)
+    msc_sp_sheet(wb['МСК и СП'], info)
+    rt_sheet(wb['Rt'], info)
+    gain_7day_sheet(wb['прирост 7дн'], info)
+    delta_day_sheet(wb['дельта за сутки'], info)
+    daily_region_gain_sheet(wb['По рег прис (сут)'], info)
+
+    # if info['last_date'].weekday()+1 == WEEKLY_REPORT_DAY:
+    #     weekly_gain_sheet(wb['Прирост нед'], info)
+    #     tpr_weekly_gain_sheet(wb['Тпр нед прироста'], info)
+    #     weekly_region_gain_sheet(wb['По рег прис (нед)'], info)
+    #     date_week_sheet(wb['Дата-неделя'], info)
+
+    # base_rf_sheet(wb['База РФ'], info)
+
 
     wb.save('russia_regions.xlsx')
 
